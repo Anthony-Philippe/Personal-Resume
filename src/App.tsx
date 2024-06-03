1f5eea340c12b5a0f4493a9406ad6e1852f6ad01
DindonDesAlpes <63336315+Anthony-Philippe@users.noreply.github.com>
2024-06-03 14:07:12 +0200

    Adding Theme
    Add theme color to tailwind config file

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,6 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="App">
-        <header className="App-header via-pink-100 p-100 bg-10">
-          <h1>React App</h1>
-        </header>
-      </div>
     </>
   );
 }
