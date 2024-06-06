67e0032dec539f0a4099cefc5a74a9a9215260c9
DindonDesAlpes <63336315+Anthony-Philippe@users.noreply.github.com>
2024-06-06 18:11:30 +0200

    Icon socialnetwork
    Add icon for social nettwork with wave animation

---
+++ src/components/IconWave.tsx
@@ -0,0 +1,33 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+type IconLink = {
+  icon: React.ReactElement;
+  link: string;
+};
+
+type IconWaveProps = {
+  icons: IconLink[];
+};
+
+export const IconWave: React.FC<IconWaveProps> = ({ icons }) => {
+  return (
+    <div className="flex space-x-8 absolute top-8 left-4">
+      {icons.map((iconLink, index) => (
+        <motion.a
+          key={index}
+          href={iconLink.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          initial={{ y: "-100vh", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1.2, delay: index * 0.2 }}
+          className="relative text-3xl text-whitecursor-pointer"
+          whileHover={{ scale: 1.3 }}
+        >
+          {iconLink.icon}
+        </motion.a>
+      ))}
+    </div>
+  );
+};
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,12 +1,26 @@
 import rocket from "@assets/animations/rocket.json";
 import { AuroraHero } from "@components/Aurora";
 import { Hero } from "@components/Hero";
+import { IconWave } from "@components/IconWave";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import { motion } from "framer-motion";
 import Lottie from "lottie-react";
 
 export const Header = () => {
+  const icons = [
+    {
+      icon: <GitHubIcon />,
+      link: "https://github.com",
+    },
+    {
+      icon: <LinkedInIcon />,
+      link: "https://linkedin.com",
+    },
+  ];
+
   return (
-    <div className="flex flex-row h-screen items-center justify-evenly mx-36">
+    <div className="relative flex flex-row h-screen items-center justify-evenly mx-36">
       <Hero />
       <motion.div
         initial={{ y: "-100vh", opacity: 0 }}
@@ -16,6 +30,7 @@ export const Header = () => {
         <Lottie animationData={rocket} className="size-96" />
       </motion.div>
       <AuroraHero />
+      <IconWave icons={icons} />
     </div>
   );
 };
