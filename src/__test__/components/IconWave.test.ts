848fd5a7cd1d00a06f3bca6b4025848e24313c44
DindonDesAlpes <63336315+Anthony-Philippe@users.noreply.github.com>
2024-06-10 00:21:49 +0200

    Improve test
    New tests for Iconwave

--- src/__test__/components/IconWave.test.ts
+++ src/__test__/components/IconWave.test.ts
@@ -1,8 +1,32 @@
 import { IconWave } from "../../components/IconWave";
 
+jest.mock("framer-motion", () => ({
+  ...jest.requireActual("framer-motion"),
+  motion: {
+    a: ({ children }: { children: any }) => children,
+  },
+}));
+
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
+  createElement: jest.fn(),
+}));
+
+jest.mock("@mui/icons-material/GitHub", () => () => "GitHubIcon");
+jest.mock("@mui/icons-material/LinkedIn", () => () => "LinkedInIcon");
+
 describe("IconWave", () => {
-  test("should render", () => {
+  test("should render without icons", () => {
     const wrapper = IconWave({ icons: [] });
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("should render with icons", () => {
+    const icons = [
+      { icon: "GitHubIcon", link: "https://github.com" },
+      { icon: "LinkedInIcon", link: "https://linkedin.com" },
+    ];
+    const wrapper = IconWave({ icons } as any);
     expect(wrapper).toMatchSnapshot();
   });
 });
